e7474b68460fde021115baec2ffd212074b64842
Ugni Blanc <ajo2185@gmail.com>
2020-04-12 21:00:46 -0700

    added function to pull user selection in quiz

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,8 +1,8 @@
 
-
-// onclick display value of radio Selection
-
-
-
+//  Event listener for self quiz selection
+ $(document).on("click", ".radio-inline", function () {
+    var userSelection = $(this).attr('value');
+    console.log(userSelection);
+ });
 
 
